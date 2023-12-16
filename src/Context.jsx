331e67ed801b79fd1a687101870f059dd620dcd4
Rahul Bauri <kumarrahulbauri2000@gmail.com>
2023-12-17 01:01:03 +0530

    first commit

---
+++ src/Context.jsx
@@ -0,0 +1,48 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+
+const GlobalContext = createContext();
+
+export const useGlobalContext = () => useContext(GlobalContext);
+
+const getInitialDarkMode = () => {
+  const prefersDarkMode = window.matchMedia(
+    '(prefers-color-scheme: dark)'
+  ).matches;
+
+  const storedDarkMode = localStorage.getItem('darkTheme');
+
+  if (storedDarkMode === null) {
+    return prefersDarkMode;
+  }
+
+  return storedDarkMode === 'true';
+};
+
+const AppContext = ({ children }) => {
+  const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode());
+  const [searchTerm, setSearchTerm] = useState('cat');
+
+  const toggleDarkTheme = () => {
+    const newDarkTheme = !isDarkTheme;
+    setIsDarkTheme(newDarkTheme);
+
+    localStorage.setItem('darkTheme', newDarkTheme);
+
+    const body = document.querySelector('body');
+    body.classList.toggle('dark-theme', newDarkTheme);
+  };
+
+  useEffect(() => {
+    document.body.classList.toggle('dark-theme', isDarkTheme);
+  }, []);
+
+  return (
+    <GlobalContext.Provider
+      value={{ isDarkTheme, toggleDarkTheme, searchTerm, setSearchTerm }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
+
+export default AppContext;
